15956ed58729a030c0f7909ef76ef3a384b1dcb2
Austin Lowey <49540411+AustinLowey@users.noreply.github.com>
2024-03-19 19:26:28 -0400

    Added v2 random forest model and several analysis outputs. Refactored some functions.

--- src/models/ml_modeling.py
+++ src/models/ml_modeling.py
@@ -13,6 +13,39 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import KFold, cross_val_score, train_test_split
 
 
+def create_model(model_details: Dict) -> None:
+    """Main function. Creates, evaluates, and saves the model by executing all functions."""
+
+    # Unpack model details dict
+    prob_trans_func_type = model_details['prob_trans_func_type']
+    alpha = model_details['alpha']
+    model_name = model_details['model_name']
+    model_type = model_details['model_type']
+    k = model_details['k']
+    test_val_size = model_details['test_val_percentage']
+    features = model_details['features']
+
+    # Create the model by running all functions
+    df = load_pg_data()
+    df_transform = apply_prob_trans_func(df, func_type=prob_trans_func_type, alpha=alpha)
+    X_train_val, X_test, y_train_val, y_test, _, aux_test = partition_data(
+        df_transform,
+        test_size=test_val_size,
+        feature_columns=features
+    )
+    second_split_size = test_val_size / (1 - test_val_size) # i.e., percentage split of a percentage split
+    model, val_metrics = train_and_evaluate(
+        X_train_val,
+        y_train_val,
+        model_type=model_type,
+        k=k,
+        second_split_size=second_split_size
+    )
+    test_metrics, df_predictions = evaluate_test_set(model, X_test, y_test, aux_test)
+    df_predictions_w_ranking = reverse_prob_trans_func(df_predictions, func_type=prob_trans_func_type, alpha=alpha)
+    save_model_and_metrics(model, model_name, val_metrics, test_metrics, df_predictions_w_ranking, model_details)
+
+
 def load_pg_data(columns: List[str] = [
     'ranking',
     'num_idols_possessed',
@@ -41,16 +74,15 @@ def load_pg_data(columns: List[str] = [
 def apply_prob_trans_func(df, func_type='decay', alpha=0.2) -> pd.DataFrame:
     """Add column to convert ranking to a probability using a tansformation function."""
     
+    # Calculate the total number of contestants per season and merge into df
+    contestants_per_season = df.groupby('season')['ranking'].count().reset_index(name='season_total_contestants')
+    df = df.merge(contestants_per_season, on='season', how='left')
+
     if func_type == 'decay':
         df['win_probability'] = np.exp(-alpha * (df['ranking'] - 1))
 
     elif func_type == 'linear':
-        # Calculate the total number of contestants per season and merge into df
-        contestants_per_season = df.groupby('season')['ranking'].count().reset_index(name='season_total_contestants')
-        df = df.merge(contestants_per_season, on='season', how='left')
-        
         # Apply the linear transformation to calculate the probability of winning
-        # Retain intermediate column for reversing this function during analysis, if desired.
         df['win_probability'] = (df['season_total_contestants'] - df['ranking'] + 1) / df['season_total_contestants']
 
     return df
@@ -71,6 +103,7 @@ def partition_data(
             'contestant_name',
             'season',
             'ranking',
+            'season_total_contestants'
         ],
         random_state: int = 42
 ) -> Tuple:
@@ -104,7 +137,7 @@ def partition_data(
     return X_train_val, X_test, y_train_val, y_test, aux_train_val, aux_test
 
 
-def train_and_evaluate(X_train_val, y_train_val, model_type='linear', k=1, random_state=42):
+def train_and_evaluate(X_train_val, y_train_val, model_type='linear', k=1, second_split_size=0.25, random_state=42):
     """
     Trains the model and provides performance metrics.
     
@@ -119,6 +152,7 @@ def train_and_evaluate(X_train_val, y_train_val, model_type='linear', k=1, rando
     - Trained model.
     - Dictionary of performance metrics on the validation set.
     """
+
     if model_type == 'linear':
         model = LinearRegression()
     elif model_type == 'random_forest':
@@ -131,7 +165,12 @@ def train_and_evaluate(X_train_val, y_train_val, model_type='linear', k=1, rando
         val_metrics['val_mse'] = -np.mean(mse_scores)
         val_metrics['val_mse_std'] = np.std(mse_scores)
     else:
-        X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.25, random_state=random_state)
+        X_train, X_val, y_train, y_val = train_test_split(
+            X_train_val,
+            y_train_val,
+            test_size=second_split_size,
+            random_state=random_state
+        )
         model.fit(X_train, y_train)
         predictions = model.predict(X_val)
         val_metrics['val_mse'] = mean_squared_error(y_val, predictions)
@@ -139,7 +178,7 @@ def train_and_evaluate(X_train_val, y_train_val, model_type='linear', k=1, rando
         val_metrics['val_r2'] = r2_score(y_val, predictions)
     
     if model_type == 'random_forest':
-        val_metrics['feature_importances'] = model.feature_importances_
+        val_metrics['feature_importances'] = list(model.feature_importances_)
     
     return model, val_metrics
 
@@ -176,6 +215,48 @@ def evaluate_test_set(model, X_test, y_test, aux_test):
     return test_metrics, df_predictions
 
 
+def reverse_prob_trans_func(df_predictions, func_type='decay', alpha=0.2):
+    """
+    Reverses probability transformation function, adding a column to df with predicted contestant ranking.
+    
+    Parameters:
+    - df_predictions: Predictions DataFrame.
+    - func_type: The type of probability transformation function to reverse ('decay' or 'linear').
+    - alpha: The alpha parameter used in the decay transformation.
+    
+    Returns:
+    - DataFrame with a newly-added column for predicted rankings.
+    """
+
+    # Handle negative or 0 predicted probabilities
+    df_predictions['predicted_probability'] = np.where(
+        df_predictions['predicted_probability'] <= 0,
+        0.01,
+        df_predictions['predicted_probability']
+    )
+
+    if func_type == 'linear':
+        # Inverse operation for linear transformation
+        df_predictions['predicted_ranking'] = np.round(
+            (1 - df_predictions['predicted_probability']) * df_predictions['season_total_contestants'] + 1
+        )
+    
+    elif func_type == 'decay':
+        # Inverse operation for decay transformation
+        df_predictions['predicted_ranking'] = np.round(
+            np.log(df_predictions['predicted_probability']) / (-alpha) + 1
+        )
+
+    # Ensure predicted rankings do not exceed the max number of contestants in a season
+    df_predictions['predicted_ranking'] = np.where(
+        df_predictions['predicted_ranking'] > df_predictions['season_total_contestants'],
+        df_predictions['season_total_contestants'],
+        df_predictions['predicted_ranking']
+    )
+    
+    return df_predictions
+
+
 def save_model_and_metrics(model, dir_name, val_metrics, test_metrics, df_predictions, model_details):
     """
     Saves the model and performance metrics to a specified directory.
@@ -198,44 +279,72 @@ def save_model_and_metrics(model, dir_name, val_metrics, test_metrics, df_predic
     model_path = os.path.join(base_path, 'model.joblib')
     joblib.dump(model, model_path)
 
+    # Save the predictions DataFrame as a CSV file
+    predictions_path = os.path.join(base_path, 'predictions.csv')
+    df_predictions_sorted = df_predictions.sort_values(by=['season', 'ranking'], ascending=[True, True])
+    csv_col_order = [
+        'contestant_name',
+        'actual_probability_from_ranking',
+        'predicted_probability',
+        'ranking',
+        'predicted_ranking',
+        'season_total_contestants',
+        'season'
+    ]
+    df_predictions_sorted[csv_col_order].to_csv(predictions_path, index=False)
+
     # Save the model details as a JSON file
     details_path = os.path.join(base_path, 'model_details.json')
     with open(details_path, 'w') as f:
         json.dump(model_details, f, indent=2)
     
     # Combine val and test metrics, then write to a JSON file
-    all_metrics = {**val_metrics, **test_metrics}
+    all_metrics = {**test_metrics, **val_metrics}
     metrics_path = os.path.join(base_path, 'performance_metrics.json')
     with open(metrics_path, 'w') as f:
         json.dump(all_metrics, f, indent=2)
-
-    # Save the predictions DataFrame as a CSV file
-    predictions_path = os.path.join(base_path, 'predictions.csv')
-    df_predictions.to_csv(predictions_path, index=False)
     
     print(f'Model, metrics, and predictions saved to {base_path}/')
 
 
 if __name__ == "__main__":
-    # Update these values for new models:
-    func_type = 'decay'                 # Type of probability transformation function ('decay' or 'linear')
-    alpha = 0.2                         # Decay value if func_type=decay. See README for info on this value
-    model_name = 'v1_linear_regression' # Name of the folder path everything saved will go into
-    model_type = 'linear'               # Type of ML model ('linear' or 'random_forest')
-    k=4                                 # Number of folds for K-fold Cross-validation
+    """Update the model_details dict values for new models:"""
 
-    ###
+    # v1 - Linear Regression
+    """
+    model_details = {
+        'prob_trans_func_type': 'decay',       # Type of probability transformation function ('decay' or 'linear')
+        'alpha': 0.2,                          # Decay value if func_type=decay. See README for info on this value
+        'model_name': 'v1_linear_regression',  # Name of the folder path everything saved will go into
+        'model_type': 'linear',                # Type of ML model ('linear' or 'random_forest')
+        'k': 1,                                # Number of folds for K-fold Cross-validation
+        'test_val_percentage': 0.2,            # Percentage of data in test and validation partitions
+        'features': [
+            'num_idols_possessed',
+            'social_score',
+            'strategy_score',
+            'challenge_wins_per_day_lasted',
+            'confessionals_per_epi'
+        ]
+    }
+    create_model(model_details)
+    """
+
+    # v2 - Random Forest
 
     model_details = {
-        'transformation_func_type': func_type,
-        'alpha': alpha,
-        'model_name': model_name,
-        'model_type': model_type,
-        'k': k
+        'prob_trans_func_type': 'decay',
+        'alpha': 0.2,
+        'model_name': 'v2_random_forest',
+        'model_type': 'random_forest',
+        'k': 1,
+        'test_val_percentage': 0.2,
+        'features': [
+            'num_idols_possessed',
+            'social_score',
+            'strategy_score',
+            'challenge_wins_per_day_lasted',
+            'confessionals_per_epi'
+        ]
     }
-    df = load_pg_data()
-    df_transform = apply_prob_trans_func(df, func_type=func_type, alpha=alpha)
-    X_train_val, X_test, y_train_val, y_test, _, aux_test = partition_data(df_transform, test_size=0.2)
-    model, val_metrics = train_and_evaluate(X_train_val, y_train_val, model_type=model_type, k=k)
-    test_metrics, df_predictions = evaluate_test_set(model, X_test, y_test, aux_test)
-    save_model_and_metrics(model, model_name, val_metrics, test_metrics, df_predictions, model_details)
+    create_model(model_details)
